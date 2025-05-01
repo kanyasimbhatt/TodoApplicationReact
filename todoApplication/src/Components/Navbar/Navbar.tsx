bd86615ddef86722e85901646df7537f2078eb0d
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-05-01 17:27:04 +0530

    remove: remove the add tasks button from navbar:

--- todoApplication/src/Components/Navbar/Navbar.tsx
+++ todoApplication/src/Components/Navbar/Navbar.tsx
@@ -1,23 +1,11 @@
 import navbarLogo from "../../../public/images-removebg-preview.png";
 import "./Navbar.css";
-import { useNavigate } from "react-router-dom";
+
 export default function Navbar() {
-  const navigate = useNavigate();
-  function handleClick() {
-    navigate("/add-edit-product");
-  }
   return (
     <nav className="navbar-div">
-      <div className="navbar-logo-title">
-        <img src={navbarLogo} alt="navbar logo" className="navbar-logo" />
-        <h2>Manager</h2>
-      </div>
-
-      <div className="add-products">
-        <button className="navbar-button" onClick={handleClick}>
-          Add Product
-        </button>
-      </div>
+      <img src={navbarLogo} alt="navbar logo" className="navbar-logo" />
+      <h2>Manager</h2>
     </nav>
   );
 }
