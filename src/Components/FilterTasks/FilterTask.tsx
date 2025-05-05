571110359c6c70d7c7ea8ad1192847bc981f1fff
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-05-05 14:38:52 +0530

    add: add basic UI for filter and search functionality

---
+++ src/Components/FilterTasks/FilterTask.tsx
@@ -0,0 +1,55 @@
+import "./FilterTask.css";
+
+export default function FilterTask() {
+  function handleSearchByTitle(event: React.ChangeEvent) {
+    console.log(event);
+  }
+
+  function handleSearchByDescription(event: React.ChangeEvent) {
+    console.log(event);
+  }
+
+  function handleSearchByBoth(event: React.ChangeEvent) {
+    console.log(event);
+  }
+
+  function handleFilterByStatus(event: React.ChangeEvent) {
+    console.log(event);
+  }
+  return (
+    <>
+      <div className="filter-section">
+        <input
+          type="text"
+          id="input"
+          onChange={handleSearchByTitle}
+          placeholder="Search By Title"
+        />
+        <input
+          type="text"
+          id="input"
+          onChange={handleSearchByDescription}
+          placeholder="Search By Description"
+        />
+        <input
+          type="text"
+          id="input"
+          onChange={handleSearchByBoth}
+          placeholder="Search By Title and Description"
+        />
+        <label className="status-filter">
+          Filter By Status:
+          <select
+            value={`All Tasks`}
+            id="input"
+            onChange={handleFilterByStatus}
+          >
+            <option value={`Todo`}>Todo</option>
+            <option value={`In Progress`}>In Progress</option>
+            <option value={`Done`}>Done</option>
+          </select>
+        </label>
+      </div>
+    </>
+  );
+}
--- src/Components/ShowTasks/ShowTasks.tsx
+++ src/Components/ShowTasks/ShowTasks.tsx
@@ -2,6 +2,7 @@ import "./showTasks.css";
 import { useGlobalContext } from "../viewAllTasks/ViewAllTasks";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import FilterTask from "../FilterTasks/FilterTask";
 
 export type Task = {
   id: string;
@@ -46,55 +47,58 @@ export default function ShowTasks() {
 
   const statusOptions = ["Done", "In Progress", "Todo"];
   return (
-    <div className="show-task">
-      {task.length === 0 ? (
-        <div className="header-wrapper">
-          {" "}
-          <h3>No Tasks Added yet!</h3>
-        </div>
-      ) : (
-        <></>
-      )}
-      {task.map((task: Task) => (
-        <div className={`card-wrapper ${task.status}`} key={task.id}>
-          <div className="tasks-title">{task.title}</div>
-          <div className="tasks-description">{task.description}</div>
-          <select
-            id={task.id}
-            className="options-select"
-            value={task.status}
-            onChange={handleChangeOnStatus}
-          >
-            <option disabled>{task.status}</option>
-            <option>
-              {statusOptions[0] === task.status
-                ? statusOptions[1]
-                : statusOptions[0]}
-            </option>
-            <option>
-              {statusOptions[2] === task.status
-                ? statusOptions[1]
-                : statusOptions[2]}
-            </option>
-          </select>
-          <div className="edit-delete-button">
-            <button
-              className="edit-button"
-              id={`${task.id}`}
-              onClick={handleTaskEdit}
-            >
-              Edit
-            </button>
-            <button
-              className="edit-button"
-              id={`${task.id}`}
-              onClick={handleTaskDelete}
-            >
-              Delete
-            </button>
+    <>
+      <FilterTask />
+      <div className="show-task">
+        {task.length === 0 ? (
+          <div className="header-wrapper">
+            {" "}
+            <h3>No Tasks Added yet!</h3>
           </div>
-        </div>
-      ))}
-    </div>
+        ) : (
+          <></>
+        )}
+        {task.map((task: Task) => (
+          <div className={`card-wrapper ${task.status}`} key={task.id}>
+            <div className="tasks-title">{task.title}</div>
+            <div className="tasks-description">{task.description}</div>
+            <select
+              id={task.id}
+              className="options-select"
+              value={task.status}
+              onChange={handleChangeOnStatus}
+            >
+              <option disabled>{task.status}</option>
+              <option>
+                {statusOptions[0] === task.status
+                  ? statusOptions[1]
+                  : statusOptions[0]}
+              </option>
+              <option>
+                {statusOptions[2] === task.status
+                  ? statusOptions[1]
+                  : statusOptions[2]}
+              </option>
+            </select>
+            <div className="edit-delete-button">
+              <button
+                className="edit-button"
+                id={`${task.id}`}
+                onClick={handleTaskEdit}
+              >
+                Edit
+              </button>
+              <button
+                className="edit-button"
+                id={`${task.id}`}
+                onClick={handleTaskDelete}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
