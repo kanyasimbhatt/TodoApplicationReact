341e71b0fc61b4b8427cbc6e4b16e05b72c1c225
kanyasimbhatt <kanya.bhatt@simformsolutions.com>
2025-05-07 13:15:54 +0530

    remove: remove code causing error

--- src/Components/AddOrEditTask/AddEditForm.tsx
+++ src/Components/AddOrEditTask/AddEditForm.tsx
@@ -5,7 +5,7 @@ import { z } from "zod";
 import useTask from "../../context/TaskContext/TaskContext";
 import "./AddEditForm.css";
 import { Task } from "../../types/TaskType/types";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 type StatusType = "Todo" | "In Progress" | "Done";
 
@@ -28,7 +28,6 @@ type FormFields = z.infer<typeof schema>;
 
 export default function AddEditForm({ taskId }: { taskId: string }) {
   const navigate = useNavigate();
-  const ref = useRef(true);
   const { tasks, setTasks } = useTask();
   let allTasks = JSON.parse(localStorage.getItem("tasks-array") as string);
   const taskIndex = allTasks.findIndex((task: Task) => task.id === taskId);
@@ -61,9 +60,7 @@ export default function AddEditForm({ taskId }: { taskId: string }) {
   };
 
   useEffect(() => {
-  
       localStorage.setItem("tasks-array", JSON.stringify(tasks));
- 
   }, [tasks]);
 
   return (
--- src/pages/viewAllTasks/ViewAllTasks.tsx
+++ src/pages/viewAllTasks/ViewAllTasks.tsx
@@ -1,6 +1,5 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import ShowTasks from "../../Components/ShowTasks/ShowTasks";
-import TaskProvider from "../../context/TaskContext/TaskProvider";
 
 export default function ViewAllTask() {
   return (
