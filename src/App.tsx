6718f02a81b5c472fcc8e135a256af8d9abd1bb9
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-05-08 13:47:01 +0530

    add: add fallback page for page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ViewAllTaskWrapper } from "./Components/Tasks/ViewAllTasks/ViewAllTasks";
 import { AddEditTaskWrapper } from "./Components/Tasks/AddOrEditTask/AddEditTask";
+import { NotFound } from "./Components/Tasks/NotFound/NotFound";
 function App() {
   return (
     <>
@@ -9,7 +10,7 @@ function App() {
           <Route path="/" element={<ViewAllTaskWrapper />}></Route>
           <Route path="/add-task" element={<AddEditTaskWrapper />}></Route>
           <Route path="/add-task/:id" element={<AddEditTaskWrapper />}></Route>
-          <Route path="*" element={<ViewAllTaskWrapper />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </Router>
     </>
